0f52dbab4eaa0fd767f0501c2941bdf5633bbabb
Fernando Lima <eufernandoprogramador@gmail.com>
2024-03-19 01:19:19 -0300

    fix: Corrigindo exibição da arena na questão 03 e utilizando Children + cloneElement

--- atividade01/src/components/03Batalha.jsx
+++ atividade01/src/components/03Batalha.jsx
@@ -1,17 +1,21 @@
+import { Children, cloneElement } from 'react'
+
 import style from './03Batalha.module.css'
 
-export function Hero({ name, imagem }) {
+export function Hero({ name, imagem, arena }) {
   return (
     <div className={style.fighter}>
+      <h3>Arena: {arena}</h3>
       <img src={imagem} height={200} width={200} alt="" />
       <h3>{name}</h3>
     </div>
   )
 }
 
-export function Enemy({ name, imagem }) {
+export function Enemy({ name, imagem, arena }) {
   return (
     <div className={style.fighter}>
+      <h3>Arena: {arena}</h3>
       <img src={imagem} alt="" />
       <h3>{name}</h3>
     </div>
@@ -19,11 +23,15 @@ export function Enemy({ name, imagem }) {
 }
 
 export function Arena({ arena, children }) {
-  console.log(children)
   return (
     <div className={style.arena}>
-      <h2>{arena}</h2>
-      <div className={style.fight}>{children}</div>
+      {/* <h2>{arena}</h2> */}
+      {/* <div className={style.fight}>{children}</div> */}
+      <div className={style.fight}>
+        {Children.map(children, (fighter, index) => {
+          return cloneElement(fighter, { key: index, arena })
+        })}
+      </div>
     </div>
   )
 }
